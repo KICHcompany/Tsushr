dfe69da297653dae4ace01b3fa939693fe94355a
KICHcompany <h9454448@gmail.com>
2025-10-30 02:43:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from keep_alive import start_server
 from playwright_scraper import run_bot
-import threading, time
+import threading
+import time
 
 def loop_bot():
     while True:
@@ -11,5 +12,5 @@ def loop_bot():
         time.sleep(60)
 
 if __name__ == "__main__":
-    threading.Thread(target=loop_bot).start()
+    threading.Thread(target=loop_bot, daemon=True).start()
     start_server()
